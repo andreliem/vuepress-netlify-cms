e61e6f37e82ce2aafcfe7a2759a216bf25073876
Andre Liem <andre.liem@gmail.com>
2018-05-23 15:36:58 -0700

    first test version

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Netlify + VuePress',
+  themeConfig: {
+    docsDir: 'docs',
+    repo: 'andreliem/vuepress-netlify',
+
+    nav: [
+      {
+        text: 'Welcome',
+        link: '/docs/',
+      },
+      {
+        text: 'Admin',
+        link: '/admin',
+      }
+    ]
+  }
+}
